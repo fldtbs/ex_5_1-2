d675e63d0806b02c43d35a440d2e4119b5d28f0a
JBEDU20 <JBEDU20@JBEDU-20>
2022-03-28 16:19:49 +0900

    다시

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,8 @@
 
 public abstract class Calculator {
+	
+	public abstract int add(int a, int b);
+	public abstract int sub(int a, int b);
+	public abstract double average(int a[]);
 
 }
--- src/GoodCalcu.java
+++ src/GoodCalcu.java
@@ -1,4 +1,22 @@
 
-public class GoodCalcu {
+public class GoodCalcu extends Calculator {
 
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double average(int[] a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
